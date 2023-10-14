4d870f92c0223129b15e26e5e25295baa8cd01e5
Hung X. Tran <hungxtran@icloud.com>
2023-10-14 22:20:56 +0700

    update info.plist and change to new pub

--- qr_to_google_sheets/lib/main.dart
+++ qr_to_google_sheets/lib/main.dart
@@ -1,19 +1,71 @@
+import 'package:ai_barcode_scanner/ai_barcode_scanner.dart';
 import 'package:flutter/material.dart';
-// import 'package:mobile_scanner/mobile_scanner.dart';
 
 void main() {
-  runApp(const MainApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String barcode = 'Tap  to scan';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(' Scanner'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ElevatedButton(
+              child: const Text('Scan Barcode'),
+              onPressed: () async {
+                await Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => AiBarcodeScanner(
+                      validator: (value) {
+                        return value.startsWith('https://');
+                      },
+                      canPop: false,
+                      onScan: (String value) {
+                        debugPrint(value);
+                        setState(() {
+                          barcode = value;
+                        });
+                      },
+                      onDetect: (p0) {},
+                      onDispose: () {
+                        debugPrint("Barcode scanner disposed!");
+                      },
+                      controller: MobileScannerController(
+                        detectionSpeed: DetectionSpeed.noDuplicates,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+            Text(barcode),
+          ],
         ),
       ),
     );
