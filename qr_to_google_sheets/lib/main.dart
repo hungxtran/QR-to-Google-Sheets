c9070801270818f1ee562879281c4ce99e8f3860
Hung X. Tran <hungxtran@icloud.com>
2023-11-23 22:49:08 +0700

    add static placeholders and comments

--- qr_to_google_sheets/lib/main.dart
+++ qr_to_google_sheets/lib/main.dart
@@ -1,7 +1,29 @@
 import 'package:ai_barcode_scanner/ai_barcode_scanner.dart';
 import 'package:flutter/material.dart';
+import 'package:gsheets/gsheets.dart';
+import 'package:flutter/cupertino.dart';
 
-import 'setttings.dart';
+// your google auth credentials
+// these can be obtain from: https://console.cloud.google.com/apis/dashboard
+const _credentials = r'''
+{
+  "type": "service_account",
+  "project_id": "YOUR_PROJECT_ID",
+  "private_key_id": "YOUR_PRIVATE_KEY_ID",
+  "private_key": "YOUR_PRIVATE_KEY",
+  "client_email": "YOUR_SERVICE_ACCOUNT_CLIENT_EMAIL",
+  "client_id": "YOUR_CLIENT_ID",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "YOUR_CLIENT_X509_CERT_URL",
+  "universe_domain": "googleapis.com"
+}
+''';
+
+// your spreadsheet id.
+// https://docs.google.com/spreadsheets/d/SPREADSHEET_ID_IS_HERE
+const _spreadsheetId = 'YOUR_SPREADSHEET_ID';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
